2eab15703efaf1b1e60dc89cc145617b2c69297e
zhaofangjia <465531729@qq.com>
2020-01-08 11:00:43 +0800

    立项

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <van-tabbar route>
+      <van-tabbar-item replace to="/home" icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item replace to="/search" icon="search">标签</van-tabbar-item>
+      <van-tabbar-item replace to="/home111" icon="home-o">标签111</van-tabbar-item>
+      <van-tabbar-item replace to="/search222" icon="search">标签222</van-tabbar-item>
+    </van-tabbar>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+body {
+  background: slategray;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <van-swipe :autoplay="1000" indicator-color="hotpink">
+      <van-swipe-item>
+        <img src="https://img.yzcdn.cn/vant/apple-1.jpg" alt />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img src="https://img.yzcdn.cn/vant/apple-2.jpg" alt />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img src="https://img.yzcdn.cn/vant/apple-3.jpg" alt />
+      </van-swipe-item>
+      <van-swipe-item>
+        <img src="https://img.yzcdn.cn/vant/apple-4.jpg" alt />
+      </van-swipe-item>
+    </van-swipe>
+    <b>Home页面</b>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  data() {
+    return {
+      images: [
+        "https://img.yzcdn.cn/vant/apple-1.jpg",
+        "https://img.yzcdn.cn/vant/apple-2.jpg"
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+img {
+  height: 200px;
+}
+</style>
